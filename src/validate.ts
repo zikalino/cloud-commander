57be4d89994a91f8b499dd70106f500e4de83377
Zim Kalinowski <zim.kalinowski@espressif.com>
2024-12-10 09:38:52 +0100

    run actual validation and print errors

--- src/validate.ts
+++ src/validate.ts
@@ -2,10 +2,15 @@ import * as helpers from '@zim.kalinowski/vscode-helper-toolkit';
 
 console.log("Validation...");
 
-var loader = new helpers.DefinitionLoader("./", "defs/____tree.yaml");
+var loader = new helpers.DefinitionLoader("./", "defs/____tree.yaml", true);
 var templates = loader.getYaml();
 
 if (templates === null) {
-  console.log("FAILED!!!");
+  var errors = loader.getErrors();
+
+  for (var i = 0; i < errors.length; i++) {
+    console.log(errors[i]);
+  }
+  
   process.exit(1);
 }
